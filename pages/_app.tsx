4e5eabdd606ba2e4afa09ca507655046e29e687e
ogeeDeveloper <omarogrant202@hotmail.com>
2022-05-19 09:17:07 -0500

    Added the next auth package and implemented inside app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {SessionProvider} from "next-auth/react"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps:{session, ...pageProps} }: AppProps) {
+  return (
+    // Wrap the entire appp so that we can use the session from the hook. This will wllow us tyo use all the next auth hooks throughout the app
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+      </SessionProvider>
+  )
 }
 
 export default MyApp
