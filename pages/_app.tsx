c2be445e64bd2565e70c44e298c25952eeff670f
ogeeDeveloper <omarogrant202@hotmail.com>
2022-05-20 08:07:15 -0500

    set up apollo client and add it to the frontend

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,21 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {SessionProvider} from "next-auth/react"
 import Header from '../components/Header'
+import {ApolloProvider} from '@apollo/client'
+import client from '../apollo-client'
 
 function MyApp({ Component, pageProps:{session, ...pageProps} }: AppProps) {
   return (
-    // Wrap the entire appp so that we can use the session from the hook. This will wllow us tyo use all the next auth hooks throughout the app
-    <SessionProvider session={session}>
-      <div className='h-screen overflow-y-scroll bg-slate-200'>
-        <Header />
-        <Component {...pageProps} />
-      </div>
-      </SessionProvider>
+    // Connect appollo client to frontend
+    <ApolloProvider client={client}>
+      {/* Wrap the entire appp so that we can use the session from the hook. This will wllow us tyo use all the next auth hooks throughout the app */}
+      <SessionProvider session={session}>
+        <div className='h-screen overflow-y-scroll bg-slate-200'>
+          <Header />
+          <Component {...pageProps} />
+        </div>
+        </SessionProvider>
+      </ApolloProvider>
   )
 }
 
